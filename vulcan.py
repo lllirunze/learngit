9f014531be47a3c4cac9736845e13e627c61a0ca
3019244266-Li Runze <Lirz3019244266@163.com>
2020-11-11 16:46:16 +0800

    add feature vulcan

---
+++ vulcan.py
@@ -0,0 +1,9 @@
+bingo = 'lrznb'
+answer = input('Please input lrz\'s favourite sentence:')
+
+while True:
+    if answer == bingo:
+        break
+    answer = input('Sorry, please input again, such as \'lrznb\'...')
+
+print('You are right, lrznb!!!')
